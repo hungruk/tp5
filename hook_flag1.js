45a89e99b4a4ef17d440143fd86ac15d0b9f6054
hungruk <96331819+hungruk@users.noreply.github.com>
2026-04-03 21:13:16 +0200

    Clean up comments in hook_flag1.js
    Remove comments explaining flag submission bypass.

--- hook_flag1.js
+++ hook_flag1.js
@@ -2,8 +2,7 @@ Java.perform(function() {
     var FlagOneActivity = Java.use("b3nac.injuredandroid.FlagOneActivity");
     FlagOneActivity.submitFlag.implementation = function(input) {
         console.log("[*] Input received by submitFlag: " + input);
-        // Contourner la vérification en retournant une valeur de drapeau correcte
-        // Remplacez 'the_real_flag' par le flag réel si vous le connaissez, ou par une valeur qui permet de passer la vérification.
+    
         return this.submitFlag("the_real_flag"); 
     };
 });
